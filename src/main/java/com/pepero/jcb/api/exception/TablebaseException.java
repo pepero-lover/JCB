b1d3b4d1717acd2883a8ff29a9ad1fe57c81ca7d
빼빼로좋아 <153907110+pepero-lover@users.noreply.github.com>
2026-08-13 13:03:35 +0900

    TablebaseException.java 삭제
    사용되지 않는 예외 제거

--- src/main/java/com/pepero/jcb/api/exception/TablebaseException.java
+++
@@ -1,7 +0,0 @@
-package com.pepero.jcb.api.exception;
-
-public class TablebaseException extends RuntimeException {
-    public TablebaseException(String message) {
-        super(message);
-    }
-}
