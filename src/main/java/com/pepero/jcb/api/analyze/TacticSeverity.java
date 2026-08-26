cc5c260fffccef6e8ff4b492d821ddea228816bb
kyubin1111 <ogyubin71@gmail.com>
2026-08-26 22:25:17 +0900

    Tactic 들 제거 및 리펙토링

--- src/main/java/com/pepero/jcb/api/analyze/TacticSeverity.java
+++
@@ -1,7 +0,0 @@
-package com.pepero.jcb.api.analyze;
-
-/**
- * If this tactic should be solved right now, the severity is {@link TacticSeverity#IMMEDIATE},
- * otherwise, the severity is {@link TacticSeverity#LATENT}
- */
-public enum TacticSeverity { IMMEDIATE, LATENT }
--- src/main/java/com/pepero/jcb/api/analyze/TacticType.java
+++
@@ -1,8 +0,0 @@
-package com.pepero.jcb.api.analyze;
-
-/**
- * Tactic types
- */
-public enum TacticType {
-    FORK, SKEWER, PIN, XRAY, BATTERY, HANGING
-}
