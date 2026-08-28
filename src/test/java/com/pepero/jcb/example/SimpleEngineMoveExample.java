49a0f482afc15acf782887640d0ce5a12919f954
pepero-lover <ogyubin11@gmail.com>
2026-08-28 11:57:22 +0900

    SimpleEngineMoveExample 에 약간의 주석 추가

--- src/test/java/com/pepero/jcb/example/SimpleEngineMoveExample.java
+++ src/test/java/com/pepero/jcb/example/SimpleEngineMoveExample.java
@@ -5,20 +5,20 @@ import com.pepero.jcb.api.enums.GameOverReason;
 
 public class SimpleEngineMoveExample {
     public static void main(String[] args) {
-        int count = 1;
-        for (int i = 0; i < count; i++) {
-            ChessGame chessGame = ChessGame.startPosition();
+        // Note : there isn't a piece position evaluation but only a piece material count on evaluation.
+        // so this engine goes bongcloud, and goes pawn to a2a3, b2b3, c2c3
 
-            while (chessGame.isGameOver() == GameOverReason.NOTGAMEOVER) {
-                int[] output = SimpleEngine.search(chessGame.getBoardSnapshot(), 6);
-                chessGame.makeMove(output[1]);
-                chessGame.setCurrentMoveEval(String.valueOf(output[0] / 100.));
-                chessGame.printBoard();
-            }
+        ChessGame chessGame = ChessGame.startPosition();
 
-            System.out.println(chessGame.isGameOver());
-            System.out.println(chessGame.getPGN());
-            System.out.println();
+        while (chessGame.isGameOver() == GameOverReason.NOTGAMEOVER) {
+            int[] output = SimpleEngine.search(chessGame.getBoardSnapshot(), 6);
+            chessGame.makeMove(output[1]);
+            chessGame.setCurrentMoveEval(String.valueOf(output[0] / 100.));
+            chessGame.printBoard();
         }
+
+        System.out.println(chessGame.isGameOver());
+        System.out.println(chessGame.getPGN());
+        System.out.println();
     }
 }
